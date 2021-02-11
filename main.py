e67a056c5393a50a1e7e76c72bd18609b30a85b4
ksoeto <76516873+ksoeto@users.noreply.github.com>
2021-02-11 16:25:31 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# Do not modify these lines
+__winc_id__ = '6eb355e1a60f48a28a0bbbd0c88d9ab4'
+__human_name__ = 'lists'
+
+# Add your code after this line
+def alphabetical_order(m):
+    """"a list of strings that represent film names"""
+m_list = ['Superman', 'Empire strikes back', 'return of the jedi']
+
+print (m_list)
+
+
+    
